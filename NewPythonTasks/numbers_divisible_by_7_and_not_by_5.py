adee8cedcaea58be11efacc25f5e43fb34133fef
Sravan6305 <golisrisravan@gmail.com>
2026-02-03 22:10:16 +0530

    initial commit

--- NewPythonTasks/numbers_divisible_by_7_and_not_by_5.py
+++ NewPythonTasks/numbers_divisible_by_7_and_not_by_5.py
@@ -2,20 +2,20 @@
 # between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-# numbers = []
-# for x in range(2000, 3200+1):
-#     if x % 7 == 0 and x % 5 != 0:
-#         numbers.append(x)
-# print(numbers)
+numbers = []
+for x in range(2000, 3200+1):
+    if x % 7 == 0 and x % 5 != 0:
+        numbers.append(x)
+print(numbers)
 
 
 # Write a program which can compute the factorial of a given numbers.
 # The results should be printed in a comma-separated sequence on a single line.
 # Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
 
-# n = int(input('enter a number: '))
-# factorial = 1
-#
-# for i in range(1, n + 1):
-#     factorial *= i
-# print(factorial)
+n = int(input('enter a number: '))
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+print(factorial)
